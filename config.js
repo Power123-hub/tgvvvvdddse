d55cf5b284cf78e75c0392f20a0088ec144cfd70
Glitch (hello-express) <none>
2020-01-30 19:18:34 +0000

    👨🌧 Checkpoint ./antigreff.json:8077021/6 ./config.js:8077021/4

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-module.exports = {
+{module.exports = {
     TOKEN: 'NjYwNTM5MTA0OTc0NDcxMTY4.Xikvfw.PjWyy5C20tlafTG8N_q5zlu1xvk',    
     YT_API_KEY: 'api ', 
     prefix: '=',
     devs: ['']
-}
+}}
