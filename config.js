d5a12e1efc7f2dfc21390303ff0f53dbaabdbf24
Glitch (hello-express) <none>
2020-03-09 09:03:23 +0000

    🚃🌇 Checkpoint ./MusicBot.js:8077021/59529 ./config.js:8077021/247

--- config.js
+++ config.js
@@ -6,9 +6,3 @@
     devs: ["607676199195508822"]
   };
 }
-
-exports.TOKEN = "MzQ1ODYwNjgwMTMxNDExOTY4.XmXnGQ.tMrWMBQIbMAKUwEMxWWUqVUU_b0"; //THIS BOT IS FOR MULTIPLE GUILD
-exports.PREFIX = "=";
-
-exports.GOOGLE_API_KEY = "AIzaSyC8GHxFJ17uRpUYuyUngZ4lrlWNV_P85-w";
-exports.COLOR = "#fffff"; //COLOR OF EMBED
