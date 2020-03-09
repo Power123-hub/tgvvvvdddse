cf350e1d060804a662cdc45ab8e6b43d8a0e6007
Glitch (hello-express) <none>
2020-03-09 09:50:30 +0000

    🚩👩‍👩‍👦 Checkpoint ./server.js:8077021/59

--- server.js
+++ server.js
@@ -146,14 +146,14 @@ client.on("message", message => {
 client.on("message", message => {
   //new msg event
   if (!message.channel.guild) return;
-  if (message.content.startsWith(prefix + "set RAINVOW")) {
+  if (message.content.startsWith(prefix + "setxom")) {
     //to create the rainbow role
-    let role = message.guild.roles.find("name", "Rainbow bot.");
+    let role = message.guild.roles.find("name", "REVA");
     if (role) return message.channel.send(`This Step Already Completed !`); //if the role already created return with this msg
     //start of create role
     if (!role) {
       rainbow = message.guild.createRole({
-        name: "Rainbow bot.", //the role will create name
+        name: "REVA", //the role will create name
         color: "#000000", //the default color
         permissions: [] //the permissions
         //end of create role
@@ -167,7 +167,7 @@ client.on("ready", () => {
   //new ready event
   setInterval(function() {
     client.guilds.forEach(g => {
-      var role = g.roles.find("name", "Rainbow bot."); //rainbow role name
+      var role = g.roles.find("name", "REVA"); //rainbow role name
       if (role) {
         role.edit({ color: "RANDOM" });
       }
