33c0c59427ca29696cb2f36fd81c72a215e427fb
Glitch (hello-express) <none>
2020-03-11 13:29:54 +0000

    🌗⛄️ Checkpoint ./server.js:9606878/667

--- server.js
+++ server.js
@@ -599,19 +599,3 @@ client.on("message", message => {
 
 
 
-
-
-
-
-
-client.on("ready", async  => {
-setInterval(function(){
-client.channels.find('id', '653191535126642697').setName("V");
-client.channels.find('id', '653191535126642697').setName("VO");
-client.channels.find('id', '653191535126642697').setName("VOI");
-client.channels.find('id', '653191535126642697').setName("VOIC");
-client.channels.find('id', '653191535126642697').setName("VOICE");
-client.channels.find('id', '653191535126642697').setName("VOICE J");
-client.channels.find('id', '653191535126642697').setName("VOICE JO");
-client.channels.find('id', '653191535126642697').setName("VOICE JOI");
-client.channels.find('id', '653191535126642697').setName("VOICE JOIN");
