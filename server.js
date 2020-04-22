675e55fe150b2e5281e8a0dfb802c30bd02a2429
Glitch (hello-express) <none>
2020-04-22 18:10:28 +0000

    ❄️⛳️ Checkpoint ./server.js:15106534/36

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ client.on("ready", async ready => {
     "!San Shex Aras📿"
   ];
   setInterval(() => {
-    var guild = client.guilds.get("692339343104147456");
+    var guild = client.guilds.get("694845715335086121");
     var reva = guild.members.get("609103253656961025");
     reva.setNickname(list[i]);
 
